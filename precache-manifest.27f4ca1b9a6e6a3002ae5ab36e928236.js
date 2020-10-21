ff5ea19a679e081ff23be3ba241130a9617426ed
Chih-Yuan Su <csu68@gatech.edu>
2020-10-21 00:21:26 -0700

    Updates

---
+++ precache-manifest.27f4ca1b9a6e6a3002ae5ab36e928236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9480ebe3f309dce68c76b978e6c6f4c0",
+    "url": "/pickwebsite/index.html"
+  },
+  {
+    "revision": "77a21c86bbc7083d682e",
+    "url": "/pickwebsite/static/css/main.0bdf875d.chunk.css"
+  },
+  {
+    "revision": "28c0826f9d2a374942cd",
+    "url": "/pickwebsite/static/js/2.12d473a5.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/pickwebsite/static/js/2.12d473a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77a21c86bbc7083d682e",
+    "url": "/pickwebsite/static/js/main.51564b12.chunk.js"
+  },
+  {
+    "revision": "68e44f04e6642ae99d7d",
+    "url": "/pickwebsite/static/js/runtime-main.a5ec839e.js"
+  }
+]);
