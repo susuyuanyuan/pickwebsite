403824bbc634ddc02cc2bce18beef04fb0fce780
Chih-Yuan Su <csu68@gatech.edu>
2020-10-21 00:24:32 -0700

    add ui

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import List from "./list";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<List />, rootElement);
